e3bf02362d987e14c7165d25d3ec1f5ae6cd06c4
aditiingle <aditiingle14@gmail.com>
2025-10-07 19:33:33 -0400

    Add Sorting feature

---
+++ ConnectionSorter.java
@@ -0,0 +1,41 @@
+import java.util.List;
+
+public class ConnectionSorter {
+    public static void sortByDuration(List<TrainConnection> list) {
+        for (int i = 0; i < list.size() - 1; i++) {
+            for (int j = 0; j < list.size() - 1 - i; j++) {
+                if (list.get(j).getDurationMinutes() > list.get(j + 1).getDurationMinutes()) {
+                    TrainConnection temp = list.get(j);
+                    list.set(j, list.get(j + 1));
+                    list.set(j + 1, temp);
+                }
+            }
+        }
+    }
+
+
+    public static void sortByFirstClassPrice(List<TrainConnection> list) {
+        for (int i = 0; i < list.size() - 1; i++) {
+            for (int j = 0; j < list.size() - 1 - i; j++) {
+                if (list.get(j).getFirstClassTicketRate() > list.get(j + 1).getFirstClassTicketRate()) {
+                    TrainConnection temp = list.get(j);
+                    list.set(j, list.get(j + 1));
+                    list.set(j + 1, temp);
+                }
+            }
+        }
+    }
+
+
+    public static void sortBySecondClassPrice(List<TrainConnection> list) {
+        for (int i = 0; i < list.size() - 1; i++) {
+            for (int j = 0; j < list.size() - 1 - i; j++) {
+                if (list.get(j).getSecondClassTicketRate() > list.get(j + 1).getSecondClassTicketRate()) {
+                    TrainConnection temp = list.get(j);
+                    list.set(j, list.get(j + 1));
+                    list.set(j + 1, temp);
+                }
+            }
+        }
+    }
+}
