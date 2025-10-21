b49f9dc9d5979b55adef2121545a342af6b70139
aditiingle <aditiingle14@gmail.com>
2025-10-21 19:28:48 -0400

    Add Reservation and Trip classes

---
+++ Trip.java
@@ -0,0 +1,16 @@
+import java.lang.invoke.StringConcatFactory;
+import java.util.ArrayList;
+
+public class Trip {
+
+    String tripId;
+    ArrayList<Reservation> reservations;
+
+    public void addReservation(Reservation reservation) {}
+
+    String  getTripId() {
+        return tripId;
+    }
+
+    //public ArrayList<Reservation> getReservations(){}
+}
